b8393adea9a22d4dc035874546dc1f3ee5c03396
unknown <rafaelparente0545@digitalcollege.internal>
2024-11-21 20:57:33 -0300

    Segundo commit

--- questionario-app/src/App.jsx
+++ questionario-app/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import AddQuestion from "./components/AddPergunta";
 import AnswerQuestions from "./components/ResponderPergunta";
 import DisplayAnswers from "./components/MostrarPerguntas";
+import "./App.css"; // Importação do arquivo CSS
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
@@ -16,8 +17,8 @@ const App = () => {
   };
 
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Questionário</h1>
+    <div >
+      <h1 className="Titulo">Questionário</h1>
       <AddQuestion onAddQuestion={handleAddQuestion} />
       <hr />
       <AnswerQuestions questions={questions} onSaveAnswers={handleSaveAnswers} />
